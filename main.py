a9abd5e4491c10fdb2dae056464168e5919dd6b9
SSR-Med <sasalazarr@unal.edu.co>
2024-01-08 18:34:35 -0500

    CORS solved

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from fastapi import Body
 from dungeon import create_dungeon
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["POST"],
+    allow_headers=["*"],
+)
 
 @app.post("/image")
 async def send_image(type: str = Body()):
